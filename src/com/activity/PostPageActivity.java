6554d2baef2f0c6dd4fac79271dfd0640828230e
Woo <woogamer@gmail.com>
2012-09-03 19:58:50 +0900

    post camera update

--- src/com/activity/PostPageActivity.java
+++ src/com/activity/PostPageActivity.java
@@ -14,8 +14,10 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -57,12 +59,11 @@ public class PostPageActivity extends SherlockActivity implements
 	private String auth_token;
 
 	private Bitmap bm;
-	
+
 	Bitmap[] image = new Bitmap[6];
 	AlertDialog mDialog;
 	int current_position = 0;
-	int sampled=0;
-	
+	int sampled = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -95,15 +96,18 @@ public class PostPageActivity extends SherlockActivity implements
 
 		mButton.setOnClickListener(this);
 	}
+	
+
+	
 
 	// Post Create Server Module
 	private class Postcreate extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void... params) {
 			try {
-//				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-//				bm.compress(CompressFormat.JPEG, 75, bos);
-//				byte[] data = bos.toByteArray();
+				// ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				// bm.compress(CompressFormat.JPEG, 75, bos);
+				// byte[] data = bos.toByteArray();
 
 				HttpClient httpClient = new DefaultHttpClient();
 				HttpPost postRequest = new HttpPost(Global.ServerUrl
@@ -142,31 +146,79 @@ public class PostPageActivity extends SherlockActivity implements
 		}
 	}
 
+	
+	
+	
 	private void doTakePhotoAction() {
 		/*
 		 * References http://2009.hfoss.org/Tutorial:Camera_and_Gallery_Demo
 		 * http
-		 * ://stackoverflow.com/questions/1050297/how-to-get-the-url-of-the-
-		 * captured -image
-		 * http://www.damonkohler.com/2009/02/android-recipes.html
+		 * ://stackoverflow.com/questions/1050297/how-to-get-the-url-of-the-captured
+		 * -image http://www.damonkohler.com/2009/02/android-recipes.html
 		 * http://www.firstclown.us/tag/android/
+		 * 
+		 * 
 		 */
+		
 
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		  Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-		// temp file url
-		String url = "DCIM/AT" + String.valueOf(System.currentTimeMillis())
-				+ ".jpg";
-		mImageCaptureUri = Uri.fromFile(new File(Environment
-				.getExternalStorageDirectory(), url));
-
-		intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
-				mImageCaptureUri);
-		intent.putExtra("return-data", true);
-		doTakeAlbumAction();
-
-		startActivityForResult(intent, PICK_FROM_CAMERA);
+		  // temp file url
+//		  String url = "DCIM/AT" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+//		  mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+//
+//		 
+//		  intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+//		  intent.putExtra("return-data", true);
+//		  doTakeAlbumAction();
+//		  
+	
+			startActivityForResult(intent, PICK_FROM_CAMERA);
+			//setDismiss(mDialog);
+		  
+		  
 	}
+	
+	
+	private AlertDialog createDialog() {
+		final View innerView = getLayoutInflater().inflate(
+				R.layout.image_crop_row, null);
+		Button camera = (Button) innerView
+				.findViewById(R.id.btn_camera_crop);
+		Button gellary = (Button) innerView
+				.findViewById(R.id.btn_gellary_crop);
+		camera.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				doTakePhotoAction();
+				//setDismiss(mDialog);
+			}
+
+			
+
+		});
+		gellary.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				doTakeAlbumAction();
+				//setDismiss(mDialog);
+			}
+
+			
+		});
+		AlertDialog.Builder ab = new AlertDialog.Builder(
+				PostPageActivity.this);
+		ab.setTitle("업로드 할 이미지 선택");
+		ab.setView(innerView);
+		return ab.create();
+	}
+	
+	private void setDismiss(AlertDialog dialog) {
+		if (dialog != null && dialog.isShowing()) {
+			dialog.dismiss();
+		}
+	}
+
+	
+	
 
 	/**
 	 * to get Image from album
@@ -185,31 +237,32 @@ public class PostPageActivity extends SherlockActivity implements
 		}
 		switch (requestCode) {
 		case CROP_FROM_CAMERA: {
-			final Bundle extras = data.getExtras();
-
-			if (extras != null) {
-				Bitmap photo = extras.getParcelable("data");
-				mPhotoImageView.setImageBitmap(photo);
-			}
-
-			File f = new File(mImageCaptureUri.getPath());
-			if (f.exists()) {
-				f.delete();
-			}
-
+			image[current_position] = (Bitmap) data.getExtras().get("data");
 			break;
 		}
 
-		case PICK_FROM_ALBUM: {
+		case PICK_FROM_ALBUM: 
+		{
+					if (data.getData() != null) {
+						Log.i("TEST", "data.getData()!=null");
 
-			mImageCaptureUri = data.getData();
-			mPhotoImageView.setImageURI(mImageCaptureUri);
+						Cursor c = getContentResolver().query(
+								Uri.parse(data.getDataString()), null, null, null,
+								null);
+						c.moveToNext();
+						String image_url = c.getString(c
+								.getColumnIndex(MediaStore.MediaColumns.DATA));
+						image[current_position] = BitmapFactory
+								.decodeFile(image_url);
+				}
+					mPhotoImageView.setImageBitmap(image[current_position]);
+					break;
 
 		}
 
 		case PICK_FROM_CAMERA: {
 			System.out.println("Fffffffffff");
-
+			
 			mPhotoImageView.setImageURI(mImageCaptureUri);
 			// Intent intent = new Intent("com.android.camera.action.CROP");
 			// intent.setDataAndType(mImageCaptureUri, "image/*");
@@ -222,43 +275,47 @@ public class PostPageActivity extends SherlockActivity implements
 			// intent.putExtra("return-data", true);
 			// startActivityForResult(intent, CROP_FROM_CAMERA);
 
+			image[current_position] = (Bitmap) data.getExtras().get("data");
+			
+			mPhotoImageView.setImageBitmap(image[current_position]);
+			
 			break;
 		}
 		}
 	}
 
+
+
 	@Override
 	public void onClick(View v) {
-		new Postcreate().execute();
-		// DialogInterface.OnClickListener cameraListener = new
-		// DialogInterface.OnClickListener() {
-		// @Override
-		// public void onClick(DialogInterface dialog, int which) {
-		// doTakePhotoAction();
-		// }
-		// };
-		//
-		// DialogInterface.OnClickListener albumListener = new
-		// DialogInterface.OnClickListener() {
-		// @Override
-		// public void onClick(DialogInterface dialog, int which) {
-		// doTakeAlbumAction();
-		// }
-		// };
-		//
-		// DialogInterface.OnClickListener cancelListener = new
-		// DialogInterface.OnClickListener() {
-		// @Override
-		// public void onClick(DialogInterface dialog, int which) {
-		// dialog.dismiss();
-		// }
-		// };
-		//
-		// new AlertDialog.Builder(this).setTitle("select the image")
-		// .setPositiveButton("take picture", cameraListener)
-		// .setNeutralButton("album", albumListener)
-		// .setNegativeButton("cancel", cancelListener).show();
+		DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				doTakePhotoAction();
+			}
+		};
+
+		DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				doTakeAlbumAction();
+			}
+		};
+
+		DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		};
+
+		new AlertDialog.Builder(this).setTitle("select the image")
+				.setPositiveButton("take picture", cameraListener)
+				.setNeutralButton("album", albumListener)
+				.setNegativeButton("cancel", cancelListener).show();
 	}
+
+	
 
 	public void setImage() {
 		mPhotoImageView.setImageURI(mImageCaptureUri);
@@ -271,6 +328,8 @@ public class PostPageActivity extends SherlockActivity implements
 						| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		return super.onCreateOptionsMenu(menu);
 	}
+
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -287,7 +346,6 @@ public class PostPageActivity extends SherlockActivity implements
 		return true;
 	}
 	
-
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		Log.i("doTake", "onSaveInstanceState");
@@ -319,15 +377,19 @@ public class PostPageActivity extends SherlockActivity implements
 			imgFlag[i] = savedInstanceState.getInt("img"+i);
 			if(imgFlag[i]==1){
 				byte[] b = savedInstanceState.getByteArray("Bitmap"+i);
-
+				
 					image[i] = BitmapFactory.decodeByteArray(b, 0, b.length);
 
 				
 			} 
 		}
 		
+		
+		
 		mPhotoImageView.setImageBitmap(image[current_position]);
 		super.onRestoreInstanceState(savedInstanceState);
 	}
+	
+
 
 }
